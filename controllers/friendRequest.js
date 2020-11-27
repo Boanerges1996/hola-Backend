eb6cec7b281de04949ba68822f12f135d8d39399
Boanerges1996 <samsonnkrumah253@gmail.com>
2020-11-28 02:42:02 +0530

    corrected

--- controllers/friendRequest.js
+++ controllers/friendRequest.js
@@ -1,12 +1,14 @@
 const FriendRequestModel = require("../model/friendRequest");
 
-const invitation = (io, data) => {
+const invitation = async (io, data) => {
   try {
     let newFriend = new FriendRequestModel({ ...data });
     await newFriend.save();
     let allFriends = await FriendRequestModel.find({
       to_id: req.body.to_id,
-    }).populate("from_id", "name email avatar").populate("to_id", "name email avatar");
+    })
+      .populate("from_id", "name email avatar")
+      .populate("to_id", "name email avatar");
     io.sockets.in(data.to_id).emit("friend-request", allFriends);
   } catch (err) {
     console.log(err);
@@ -14,18 +16,20 @@ const invitation = (io, data) => {
   }
 };
 
-const getAllFriendRequest = async(req,res)=>{
-    try{
-        const id = req.params.id
-        let friendReqs = await FriendRequestModel.find({to_id:id}).populate("from_id", "name email avatar").populate("to_id", "name email avatar")
-        res.send(friendReqs)
-    }catch(err){
-        console.log(err)
-        res.send(err)
-    }
-}
+const getAllFriendRequest = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let friendReqs = await FriendRequestModel.find({ to_id: id })
+      .populate("from_id", "name email avatar")
+      .populate("to_id", "name email avatar");
+    res.send(friendReqs);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+};
 
-module.exports ={
-    invitation,
-    getAllFriendRequest
-}
+module.exports = {
+  invitation,
+  getAllFriendRequest,
+};
